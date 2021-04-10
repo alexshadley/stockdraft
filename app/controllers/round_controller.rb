bdf19ec92639876c3967dfca070e6bb29b78c541
Trenton Potter <trpotter72@gmail.com>
2021-04-10 13:42:07 -0500

    Seed DB with in-progress test with round_id='test'

--- app/controllers/round_controller.rb
+++ app/controllers/round_controller.rb
@@ -13,7 +13,6 @@ class RoundController < ApplicationController
   def draft
     @round = Round.find(params[:id])
     @all_tickers = Tickers.get_tickers
-    RoundHelper.generate_fake_draft_data(@round.round_id, @all_tickers[0..2])
     @drafted_tickers = RoundHelper.drafted_tickers(@round.round_id)
     for draft in @drafted_tickers
       puts draft.symbol

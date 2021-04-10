11eee7d367f415187ddee5dc4d0a1bcbc4bee862
Alex Shadley <alex@rideos.ai>
2021-04-10 08:53:33 -0500

    adds endpoints for creating and viewing rounds

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'round/create'
+  post 'round/create', to: 'round#create_post'
+  get 'round/:id', to: 'round#show'
+  get 'round/:id/draft', to: 'round#draft'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

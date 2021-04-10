86184fc43b9e8bab2cacdcddeb7df25c7c0313a7
Trenton Potter <trpotter72@gmail.com>
2021-04-10 13:15:26 -0500

    Pull tickers to FE, create mock draft data

--- app/controllers/round_controller.rb
+++ app/controllers/round_controller.rb
@@ -12,7 +12,13 @@ class RoundController < ApplicationController
   # Drafting Stocks
   def draft
     @round = Round.find(params[:id])
-    @tickers = Tickers.get_tickers
+    @all_tickers = Tickers.get_tickers
+    RoundHelper.generate_fake_draft_data(@round.round_id, @all_tickers[0..2])
+    @drafted_tickers = RoundHelper.drafted_tickers(@round.round_id)
+    for draft in @drafted_tickers
+      puts draft.symbol
+      puts draft.user_id
+    end
   end
 
   # Dashboard

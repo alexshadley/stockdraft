36bab7e9a335430521c26200f20e2afb2032448a
Alex Shadley <alex@rideos.ai>
2021-04-11 13:31:38 -0500

    refactors charts a bit

--- app/controllers/round_controller.rb
+++ app/controllers/round_controller.rb
@@ -28,35 +28,11 @@ class RoundController < ApplicationController
     @maxes = []
 
     prices = AlpacaReader.get_data(Time.now - 7.days)
+
+    @portfolios_by_user = @users.map{|user| [user, portfolio_series_for_user(user, @drafts, prices)]}
     
     if params[:portfolio].nil?
-      @chart_data = []
-      @portfolios = []
-
-      for user in @users
-        symbols = @drafts.select{|draft| draft.user_id == user.user_id}.map{|draft| draft.symbol}
-        portfolio_prices = prices.select{|k, v| symbols.include? k}
-
-        if portfolio_prices.size < 1
-          next 
-        end
-
-        puts portfolio_prices
-
-        summed_portfolio = {}
-        for timestamp in portfolio_prices.values[0].keys
-          sum = 0
-          for price_series in portfolio_prices.values
-            if not price_series[timestamp].nil?
-              sum += price_series[timestamp]
-            end
-          end
-          summed_portfolio[timestamp.strftime('[%B %d, %I:%M]')] = sum
-        end
-
-        @chart_data.append({name: user.display_name, data: summed_portfolio})
-        @portfolios.append({user: user, value: summed_portfolio[summed_portfolio.keys.last]})
-      end
+      @chart_data = @portfolios_by_user.map{|user, portfolio_series| {name: user.display_name, data: portfolio_series}}
 
     else
       symbols = @drafts.select{|draft| draft.user_id.to_s == params[:portfolio]}.map{|draft| draft.symbol}
@@ -72,5 +48,27 @@ class RoundController < ApplicationController
         {name: symbol, data: normalized}
       }
     end
+  end
+
+  def portfolio_series_for_user(user, drafts, all_prices)
+    symbols = drafts.select{|draft| draft.user_id == user.user_id}.map{|draft| draft.symbol}
+    portfolio_prices = all_prices.select{|k, v| symbols.include? k}
+
+    if portfolio_prices.size < 1
+      return {}
+    end
+
+    summed_portfolio = {}
+    for timestamp in portfolio_prices.values[0].keys
+      sum = 0
+      for price_series in portfolio_prices.values
+        if not price_series[timestamp].nil?
+          sum += price_series[timestamp]
+        end
+      end
+      summed_portfolio[timestamp.strftime('[%B %d, %I:%M]')] = sum
+    end
+
+    return summed_portfolio
   end
 end

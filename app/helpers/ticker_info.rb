5a61c5f656d3d49cbc7a8b1a8d44d4a7e2f16377
marshingjay <jam605@gmail.com>
2021-04-11 16:02:56 -0500

    swindled IEX

--- app/helpers/ticker_info.rb
+++ app/helpers/ticker_info.rb
@@ -1,15 +1,19 @@
 require 'iex-ruby-client'
 
 class TickerInfo
-  @@client = client = IEX::Api::Client.new(
+  sandbox_client = IEX::Api::Client.new(
+    publishable_token: ENV['IEX_SANDBOX_KEY'],
+    secret_token: ENV['IEX_SANDBOX_SECRET'],
     endpoint: 'https://sandbox.iexapis.com/v1'
   )
+  @@ticker_company_names = Tickers.get_tickers.map{|ticker| [ticker, sandbox_client.company(ticker).company_name]}.to_h
+
   
   def self.get_company_name(symbol)
-    return @@client.company(symbol).company_name
+    return @@ticker_company_names[symbol]
   end
 
   def self.get_logo_url(symbol)
-    return 'https://s3.polygon.io/logos/' + symbol.downcase + "/logo.png"
+    return "https://storage.googleapis.com/iexcloud-hl37opg/api/logos/" + symbol + ".png"
   end
 end

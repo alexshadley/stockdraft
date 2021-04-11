d3742c99a3765bec8ccf51ff56345a0b9cb2f648
Alex Shadley <alex@rideos.ai>
2021-04-11 00:51:55 -0500

    sick chart

--- app/controllers/round_controller.rb
+++ app/controllers/round_controller.rb
@@ -23,5 +23,19 @@ class RoundController < ApplicationController
     @round = Round.find(params[:id])
     @users = User.where(round_id: @round.round_id)
     @drafts = StockDraft.where(round_id: @round.round_id)
+
+    @mins = []
+    @maxes = []
+
+    values = AlpacaReader.get_data(Time.now - 5.days)
+    @chart_data = values.map{ |symbol, data|
+      first_value = data.values[0]
+      normalized = data.map{ |time, value| [time.strftime('[%B %d, %I:%M]'), value / first_value]}.to_h
+
+      @mins.append(normalized.values.min)
+      @maxes.append(normalized.values.max)
+      
+      {name: symbol, data: normalized}
+    }
   end
 end
